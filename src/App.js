9f710da5268ecc55cf01323f7ae56af4d9cf039b
priyanka08dalal <55490892+priyanka08dalal@users.noreply.github.com>
2021-01-27 00:50:16 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+// import Header from './Header'
+import ItemsButton from './components/ItemsButton'
+// import PeopleList from './components/ItemsButton'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                {/* <Header /> */}
+                <ItemsButton />
+            </div>
+        );
+    }
+}
+
+export default App;
